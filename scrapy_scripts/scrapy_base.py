4c73b5c968c54a1632b82201e7556fb18fbf228a
richardvandijk2504 <richardvandijk2504@gmail.com>
2026-01-02 01:13:45 +0100

    Update scrapy_base.py

--- scrapy_scripts/scrapy_base.py
+++ scrapy_scripts/scrapy_base.py
@@ -2,18 +2,12 @@ import scrapy
 
 def create_script(name, urls, debug):
     """
-    Function that generates Spider classes for a given name and start url(s).
-    
-    Takes inputs name and urls.
+    Generates Spider classes for a given name and start url(s).
     """
     class DynamicSpiderClass(scrapy.Spider):
         """
         Inherited Scrapy "Spider" class, sends requests, follows links,
-        handles responses, and exports results.
-
-        Class requires a name and start_urls variable in the class to function.
-        Returns a Spider class that can be made to "crawl" through the HTML file
-        of the urls and parses the info w/ the CSS 
+        handles responses, and exports results. Returns a Spider class.
         """
         def start_requests(self):
             for url in self.start_urls:
@@ -33,7 +27,6 @@ def create_script(name, urls, debug):
                     "citations": item.get("cited_by_count")
                 }
 
-    #Define Class Attributes
     DynamicSpiderClass.name = name
     DynamicSpiderClass.start_urls = urls
     DynamicSpiderClass.debug = debug
